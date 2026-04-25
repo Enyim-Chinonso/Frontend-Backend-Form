d92647fa1ffe0746f37ec481add68ca1ba773683
Enyim Chinonso <enyimchinonso23@gmail.com>
2026-04-25 16:24:58 +0100

    first commit

---
+++ Script/script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('registerForm');
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    const message = document.getElementById('message');
+
+// Password check
+if (password !== confirmPassword) {
+  message.innerHTML = `<span class="text-danger">Passwords do not match!</span>`;
+  return;
+}
+
+     try {
+    const response = await fetch("http://localhost:9000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, email, password })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      message.innerHTML = `<span class="text-success">Registration successful!</span>`;
+      form.reset();
+    } else {
+      message.innerHTML = `<span class="text-danger">${data.message}</span>`;
+    }
+
+  } catch (error) {
+    message.innerHTML = `<span class="text-danger">Server error</span>`;
+  }
+});
